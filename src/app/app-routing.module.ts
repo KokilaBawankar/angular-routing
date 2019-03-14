dea70acab0020bc92f8f00962f0eda3e355f0e17
kokila <kokila.bawankar@techprimelab.com>
2019-03-14 14:30:36 +0530

    clear clutter.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-import {ActorsComponent} from './actor-actress/actors/actors.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {ActorActressDetailsComponent} from './actor-actress/actor-actress-details/actor-actress-details.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { ActorsComponent } from './actor-actress/actors/actors.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ActorActressCardComponent } from './actor-actress/actor-actress-card/actor-actress-card.component';
-import { ActorActressDetailsComponent } from './actor-actress/actor-actress-details/actor-actress-details.component';
 import {MoviesModule} from './movies/movies.module';
 import {ActorActressModule} from './actor-actress/actor-actress.module';
 
