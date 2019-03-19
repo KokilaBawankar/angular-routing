df24200618f0c04b9347ea2be5d74654e8e9e30d
kokila <kokila.bawankar@techprimelab.com>
2019-03-19 10:56:23 +0530

    Made private authService property public to be accessible inside .html

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   username = 'Username';
   password = 'Password';
-  constructor(private authService: AuthService,
+  constructor(public authService: AuthService,
               private router: Router) { }
 
   ngOnInit() {
